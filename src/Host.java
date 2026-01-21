b129d0584dabb5f4c4f7e54a7ed8d622df1f187d
lillymarieee04 <lillywat2023@gmail.com>
2026-01-21 12:32:57 -0500

    Starter

---
+++ src/Host.java
@@ -0,0 +1,11 @@
+//A host is always waiting for the user to enter a short message together with the
+//intended receiver. The host then generates a virtual frame.
+
+//The host then creates a UDP packet to carry the virtual frame and sends it to the
+//connected switch using the switch’s IP address and port number.
+
+
+//2 threads (while true loops)
+
+public class Host {
+}
---
+++ src/Switch.java
@@ -0,0 +1,8 @@
+//A switch has multiple virtual ports, and each virtual port should be “named” using the
+//IP address and the port number of the neighbor switch or host.
+
+//For debugging purposes, the switch must print out the entire switch table every time a
+//new entry is added
+
+public class Switch {
+}
